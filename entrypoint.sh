3fc10eaa96c63620f9eda1f1fc492f6969b27896
VuTK-4823 <nguyenthienlevu04@gmail.com>
2025-11-10 18:13:33 +0700

    Disable NODE_TLS_REJECT_UNAUTHORIZED export
    Comment out NODE_TLS_REJECT_UNAUTHORIZED export for security.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Running DB migrations..."
 # Node/pg cần ssl khi nói chuyện Railway
-export NODE_TLS_REJECT_UNAUTHORIZED=0
+# export NODE_TLS_REJECT_UNAUTHORIZED=0
 npm run migration:run
 
 echo "Start app..."
